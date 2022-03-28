0abdba10c54ad149331eba908895640f9176e4ed
Mateus Pardini <mateuspardini6@gmail.com>
2022-03-28 15:14:51 -0300

    fix: Responsividade

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,91 +1,110 @@
 import React from "react";
-import { ContainerHeader, Logo } from "./Styles";
+import { ContainerHeader, BotoesHeader, MenuHeader, Logo } from "./Styles";
 import Button from "../../styles/Button";
 import logoGuilherme from "./../../assets/logoGuilherme.png";
 import logoEscrita from "./../../assets/logoEscrita.png";
 import { CgProfile } from "react-icons/cg";
 import { useHistory } from "react-router-dom";
-
+import { Menu } from "antd";
+import { AppstoreOutlined } from "@ant-design/icons";
+import { GiHamburgerMenu } from "react-icons/gi";
 function Header() {
   const history = useHistory();
+  const SubMenu = Menu.SubMenu;
+
   return (
     <ContainerHeader>
+      <MenuHeader></MenuHeader>
       <Logo>
         <img src={logoGuilherme} className="logo1" alt="logoGuilherme"></img>
         <img src={logoEscrita} className="logo2" alt="logoEscrita"></img>
       </Logo>
-      <Button
-        fontSizeMedia1080="1rem"
-        backgroundColor="transparent"
-        borderColor="transparent"
-        color="#ffffff"
-        fontSize="1.25rem"
-        onClick={() => {
-          console.log("vasco");
-          history.push("/web/homemedico");
-        }}
-      >
-        Home
-      </Button>
-      <Button
-        fontSizeMedia1080="1rem"
-        backgroundColor="transparent"
-        borderColor="transparent"
-        color="#ffffff"
-        fontSize="1.25rem"
-        onClick={() => {
-          history.push("/web/listadeusuarios");
-        }}
-      >
-        Lista de Usuários
-      </Button>
-      <Button
-        fontSizeMedia1080="1rem"
-        backgroundColor="transparent"
-        borderColor="transparent"
-        color="#ffffff"
-        fontSize="1.25rem"
-        onClick={() => {
-          history.push("/web/homemedico");
-        }}
-      >
-        Agendamentos
-      </Button>
-      <Button
-        fontSizeMedia1080="1rem"
-        backgroundColor="transparent"
-        borderColor="transparent"
-        color="#ffffff"
-        fontSize="1.25rem"
-        onClick={() => {
-          history.push("/web/homemedico");
-        }}
-      >
-        Formulários
-      </Button>
-      <Button
-        fontSizeMedia1080="1rem"
-        backgroundColor="transparent"
-        borderColor="transparent"
-        color="#ffffff"
-        fontSize="1.25rem"
-        onClick={() => {
-          history.push("/web/criacaoformulario");
-        }}
-      >
-        Chat
-      </Button>
-      <Button
-        fontSizeMedia1080="1rem"
-        backgroundColor="green"
-        borderColor="transparent"
-        color="#ffffff"
-        onClick={() => {
-          history.push("/login");
-        }}
-      >
-        <CgProfile size="2em" />
-      </Button>
+      <BotoesHeader>
+        <Button
+          fontSizeMedia1080="1rem"
+          backgroundColor="transparent"
+          borderColor="transparent"
+          color="#ffffff"
+          fontSize="1.1rem"
+          onClick={() => {
+            console.log("vasco");
+            history.push("/web/homemedico");
+          }}
+        >
+          Home
+        </Button>
+        <Button
+          fontSizeMedia1080="1rem"
+          backgroundColor="transparent"
+          borderColor="transparent"
+          color="#ffffff"
+          fontSize="1.1rem"
+          onClick={() => {
+            history.push("/web/criacaoformulario");
+          }}
+        >
+          Receitas
+        </Button>
+        <Button
+          fontSizeMedia1080="1rem"
+          backgroundColor="transparent"
+          borderColor="transparent"
+          color="#E4E6F4"
+          fontSize="1.1rem"
+          onClick={() => {
+            history.push("/web/listadeusuarios");
+          }}
+        >
+          Lista de Usuários
+        </Button>
+        <Button
+          fontSizeMedia1080="1rem"
+          backgroundColor="transparent"
+          borderColor="transparent"
+          color="#ffffff"
+          fontSize="1.1rem"
+          onClick={() => {
+            history.push("/web/homemedico");
+          }}
+        >
+          Agendamentos
+        </Button>
+        <Button
+          fontSizeMedia1080="1rem"
+          backgroundColor="transparent"
+          borderColor="transparent"
+          color="#ffffff"
+          fontSize="1.1rem"
+          onClick={() => {
+            history.push("/web/homemedico");
+          }}
+        >
+          Formulários
+        </Button>
+        <Button
+          fontSizeMedia1080="1rem"
+          backgroundColor="transparent"
+          borderColor="transparent"
+          color="#ffffff"
+          fontSize="1.1rem"
+          onClick={() => {
+            history.push("/web/criacaoformulario");
+          }}
+        >
+          Chat
+        </Button>
+        <Button
+          fontSizeMedia1080="1rem"
+          backgroundColor="green"
+          borderColor="transparent"
+          color="#ffffff"
+          onClick={() => {
+            history.push("/login");
+          }}
+        >
+          <CgProfile size="2em" />
+        </Button>
+      </BotoesHeader>
     </ContainerHeader>
   );
 }
--- src/components/Header/Styles.js
+++ src/components/Header/Styles.js
@@ -10,12 +10,17 @@ export const ContainerHeader = styled.div`
   padding-left: 5%;
   padding-right: 5%;
 `;
-export const HeaderPagina = styled.div`
+export const BotoesHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  width: 100%;
   @media (max-width: 780px) {
     display: none;
   }
 `;
 export const Logo = styled.div`
+  display: flex;
   width: 35%;
 
   .logo1 {
@@ -25,13 +30,49 @@ export const Logo = styled.div`
     width: 80%;
   }
 
-  @media (max-width: 780px) {
+  @media (max-width: 1080px) {
+    display: flex;
     width: 15%;
     .logo1 {
-      width: 65%;
+      width: 55%;
     }
     .logo2 {
       display: none;
     }
+  }
+  @media (max-width: 780px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    width: 50%;
+
+    .logo1 {
+      width: 20%;
+    }
+    .logo2 {
+      display: flex;
+      width: 100%;
+    }
+  }
+  @media (max-width: 380px) {
+    width: 100%;
+
+    .logo1 {
+      width: 15%;
+    }
+    .logo2 {
+      display: flex;
+      width: 75%;
+    }
+  }
+`;
+export const MenuHeader = styled.div`
+  display: none;
+  width: 10%;
+
+  @media (max-width: 780px) {
+    width: 10%;
+    display: flex;
   }
 `;
