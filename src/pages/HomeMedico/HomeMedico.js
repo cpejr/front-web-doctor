fdf3d9583f7acc64c8c6c9e1f39eff277deec5a6
Mateus Pardini <mateuspardini6@gmail.com>
2022-03-28 21:17:24 -0300

    feat: Pagina home adm

--- src/pages/HomeMedico/HomeMedico.js
+++ src/pages/HomeMedico/HomeMedico.js
@@ -1,11 +1,136 @@
 import React from "react";
+import Button from "../../styles/Button";
+import {
+  ContainerHome,
+  Botoes,
+  Secretarios,
+  ContainerBotoes,
+  Formulario,
+  ContainerSuperior,
+  ContainerFormulario,
+  ContainerSecretario,
+  Dados,
+  InfoSecretario,
+} from "./Styles";
+import { PlusCircleOutlined } from "@ant-design/icons";
 
 function HomeMedico() {
   return (
-    <div>
-      <h1>HomeMedico</h1>
-    </div>
+    <ContainerHome>
+      <ContainerSuperior>
+        <ContainerSecretario>
+          <Secretarios>
+            Nova(o) secretária(o) aguardando informação:
+            <Dados>
+              <Botoes>
+                <InfoSecretario>Adrianus Babaca Vieira</InfoSecretario>
+                <InfoSecretario>adrianus@babaca.com.br</InfoSecretario>
+              </Botoes>
+              <Botoes>
+                <Button
+                backgroundColor="#FDF1D1"
+                color="black"
+                width="100%"
+                borderColor="black"
+                fontSize="1em"
+                gap="1%"
+                >Aceitar Solicitação</Button>
+                <Button
+                backgroundColor="#FDF1D1"
+                color="black"
+                width="100%"
+                borderColor="black"
+                fontSize="1em"
+                gap="1%"
+                >Negar Solicitação</Button>
+              </Botoes>
+            </Dados>
+          </Secretarios>
+          <Secretarios>
+            Nova(o) secretária(o) aguardando informação:
+            <Dados>
+              <Botoes>
+                <InfoSecretario>Adrianus Babaca Vieira</InfoSecretario>
+                <InfoSecretario>adrianus@babaca.com.br</InfoSecretario>
+              </Botoes>
+              <Botoes>
+                <Button
+                backgroundColor="#FDF1D1"
+                color="black"
+                width="100%"
+                borderColor="black"
+                fontSize="1em"
+                gap="1%"
+                >Aceitar Solicitação</Button>
+                <Button
+                backgroundColor="#FDF1D1"
+                color="black"
+                width="100%"
+                borderColor="black"
+                fontSize="1em"
+                gap="1%"
+                >Negar Solicitação</Button>
+              </Botoes>
+            </Dados>
+          </Secretarios>
+        </ContainerSecretario>
+        <ContainerFormulario>
+          <Formulario>Formulário 1</Formulario>
+          <Formulario>Formulário 2</Formulario>
+          <Formulario>Formulário 3</Formulario>
+          <Formulario>Formulário 4</Formulario>
+          
+        </ContainerFormulario>
+      </ContainerSuperior>
+      <ContainerBotoes>
+        <Botoes>
+          <Button
+            backgroundColor="#E9EBFC"
+            color="#151B57"
+            width="100%"
+            borderColor="#151B57"
+            fontSize="1em"
+            gap="1%"
+          >
+            Editar Conteúdo do Aplicativo
+          </Button>
+          <Button
+            backgroundColor="#E9EBFC"
+            color="#151B57"
+            width="100%"
+            borderColor="#151B57"
+            fontSize="1em"
+            gap="1%"
+          >
+            Avaliação do Aplicativo
+          </Button>
+        </Botoes>
+        <Botoes>
+          <Button
+            backgroundColor="#E9EBFC"
+            color="#151B57"
+            width="100%"
+            borderColor="#151B57"
+            fontSize="1em"
+            gap="1%"
+          >
+            Cadastrar Novo Paciente
+            <PlusCircleOutlined style={{ color: "#151B57" }} />
+          </Button>
+          <Button
+            backgroundColor="#E9EBFC"
+            color="#151B57"
+            width="100%"
+            borderColor="#151B57"
+            fontSize="1em"
+            gap="1%"
+          >
+            Cadastrar Nova Secretária
+            <PlusCircleOutlined style={{ color: "#151B57" }} />
+          </Button>
+        </Botoes>
+      </ContainerBotoes>
+    </ContainerHome>
   );
 }
-
 export default HomeMedico;
---
+++ src/pages/HomeMedico/Styles.js
@@ -0,0 +1,98 @@
+import styled from "styled-components";
+
+export const ContainerHome = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ContainerSuperior = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+  padding-left: 5%;
+  padding-right: 5%;
+  gap: 2%;
+  margin-bottom: 5%;
+`;
+export const ContainerSecretario = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 300px;
+  align-items: center;
+  justify-content: center;
+  background-color: #eaebf8;
+  border-color: "black";
+  border-style: solid;
+  border-radius: 3px;
+  padding: 2%;
+  overflow:auto ;
+`;
+export const ContainerFormulario = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 300px;
+  align-items: center;
+  justify-content: center;
+  background-color: #eaebf8;
+  border-color: "black";
+  border-style: solid;
+  border-radius: 3px;
+  padding: 2%;
+  overflow:auto ;
+`;
+export const Secretarios = styled.div`
+  width: 100%;
+  height: 100%;
+  margin-bottom:2% ;
+`;
+
+export const Formulario = styled.div`
+  
+  border-radius: 3px;
+  border-style: solid;
+  border-color:#CFD3F8;
+  background-color:#DCDFF9;
+  width: 100%;
+  height: 100%;
+  padding: 2%;
+  margin-bottom:1% ;
+`;
+
+export const Botoes = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  padding-left: 5%;
+  padding-right: 5%;
+`;
+export const ContainerBotoes = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  width:100%;
+  gap:2%;
+`;
+export const Dados = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+export const InfoSecretario = styled.div`
+  color: black;
+  background-color: #fdf1d1;
+  width: 100%;
+  border-color: "black";
+  border-style: solid;
+  border-radius: 1px;
+  padding:2%;
+`;
