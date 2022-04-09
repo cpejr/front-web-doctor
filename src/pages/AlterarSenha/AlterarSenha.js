125d2ca7f511b56bc09bf2387ccb1c58e9485b10
Adrianus Alexsander <adrianusvieira@cpejr.com.br>
2022-04-09 00:27:16 -0300

    feat: fução de alterar layout da página

--- src/pages/AlterarSenha/AlterarSenha.js
+++ src/pages/AlterarSenha/AlterarSenha.js
@@ -1,77 +1,163 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Input from "../../styles/Input";
 import { useHistory } from "react-router-dom";
 import Button from "../../styles/Button";
-import { Conteudo, Caixa, InputVertical, BotoesMesmaLinha, Titulo } from "./Styles";
+import {
+  Conteudo,
+  Caixa,
+  InputVertical,
+  BotoesMesmaLinha,
+  Titulo,
+} from "./Styles";
 
 function AlterarSenha() {
   const history = useHistory();
+
+  const [confirmandoSenha, setConfirmandoSenha] = useState(true);
+  const [confirmouSenha, setConfirmouSenha] = useState(false);
+  const [alterador, setAlterador] = useState(true);
+
+  useEffect(() => {
+    if (alterador === true) {
+      setConfirmandoSenha(true);
+      setConfirmouSenha(false);
+    } else if (alterador === false) {
+      setConfirmandoSenha(false);
+      setConfirmouSenha(true);
+    }
+  }, [alterador]);
+
+  const [senhaAtual, setSenhaAtual] = useState("");
+
+  const [novaSenha, setNovaSenha] = useState("");
+  const [confirmarSenha, setConfirmarSenha] = useState("");
+
   return (
     <div>
-      <Conteudo>
-        <Caixa>
-          <Titulo>Alterar Senha:</Titulo>
-          <InputVertical>
-            <Input
-              placeholder="Defina sua nova senha"
-              backgroundColor="#E4E6F4"
-              borderColor="#151B57"
-              color="black"
-              fontSize="1em"
-              width="100%"
-              marginTop="2%"
-              type="password"
-              name="senha"
-            ></Input>
-            <Input
-              placeholder="Confirme sua nova senha"
-              backgroundColor="#E4E6F4"
-              borderColor="#151B57"
-              color="black"
-              fontSize="1em"
-              width="100%"
-              marginTop="5%"
-              type="password"
-              name="confirmarSenha"
-            ></Input>
-          </InputVertical>
-          <BotoesMesmaLinha>
-            <Button
-              width="40%"
-              height="50px"
-              backgroundColor="#FFFFFF"
-              borderColor="rgba(255, 0, 0, 0.25)"
-              color="#8D8D8D"
-              fontSize="1.5em"
-              fontWeight="bold"
-              fontSizeMedia="1.2em"
-              fontSizeMedia950="1.1em"
-              boxShadow="0 4px 2px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
-              onClick={() => history.push("/web/editarperfil")}
-            >
-              CANCELAR
-            </Button>
-            <Button
-              height="50px"
-              width="40%"
-              backgroundColor="#434B97"
-              borderColor="#151B57"
-              color="white"
-              fontSize="1.5em"
-              fontWeight="bold"
-              fontSizeMedia="1.2em"
-              fontSizeMedia950="1.1em"
-              boxShadow="0 4px 2px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
-              onClick={() => history.push("/web/perfil")}
-            >
-              ENTRAR
-            </Button>
-          </BotoesMesmaLinha>
-        </Caixa>
-      </Conteudo>
+      {confirmandoSenha && (
+        <div>
+          <Conteudo>
+            <Caixa>
+              <Titulo>Alterar Senha:</Titulo>
+              <InputVertical>
+                <Input
+                  placeholder="Confirme sua senha atual"
+                  backgroundColor="#E4E6F4"
+                  borderColor="#151B57"
+                  color="black"
+                  fontSize="1em"
+                  width="100%"
+                  marginTop="2%"
+                  type="password"
+                  name="senhaatual"
+                  onChange={(e) => setSenhaAtual(e.target.value)}
+                ></Input>
+              </InputVertical>
+              <BotoesMesmaLinha>
+                <Button
+                  width="40%"
+                  height="50px"
+                  backgroundColor="#FFFFFF"
+                  borderColor="rgba(255, 0, 0, 0.25)"
+                  color="#8D8D8D"
+                  fontSize="1.5em"
+                  fontWeight="bold"
+                  fontSizeMedia="1.2em"
+                  fontSizeMedia950="1.1em"
+                  boxShadow="0 4px 2px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+                  onClick={() => history.push("/web/editarperfil")}
+                >
+                  CANCELAR
+                </Button>
+                <Button
+                  height="50px"
+                  width="40%"
+                  backgroundColor="#434B97"
+                  borderColor="#151B57"
+                  color="white"
+                  fontSize="1.5em"
+                  fontWeight="bold"
+                  fontSizeMedia="1.2em"
+                  fontSizeMedia950="1.1em"
+                  boxShadow="0 4px 2px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+                  onClick={() => setAlterador(false)}
+                >
+                  CONFIRMAR
+                </Button>
+              </BotoesMesmaLinha>
+            </Caixa>
+          </Conteudo>
+        </div>
+      )}
+      {confirmouSenha && (
+        <div>
+          <Conteudo>
+            <Caixa>
+              <Titulo>Alterar Senha:</Titulo>
+              <InputVertical>
+                <Input
+                  placeholder="Defina sua nova senha"
+                  backgroundColor="#E4E6F4"
+                  borderColor="#151B57"
+                  color="black"
+                  fontSize="1em"
+                  width="100%"
+                  marginTop="2%"
+                  type="password"
+                  name="senha"
+                  onChange={(e) => setNovaSenha(e.target.value)}
+                ></Input>
+                <Input
+                  placeholder="Confirme sua nova senha"
+                  backgroundColor="#E4E6F4"
+                  borderColor="#151B57"
+                  color="black"
+                  fontSize="1em"
+                  width="100%"
+                  marginTop="5%"
+                  type="password"
+                  name="confirmarSenha"
+                  onChange={(e) => setConfirmarSenha(e.target.value)}
+                ></Input>
+              </InputVertical>
+              <BotoesMesmaLinha>
+                <Button
+                  width="40%"
+                  height="50px"
+                  backgroundColor="#FFFFFF"
+                  borderColor="rgba(255, 0, 0, 0.25)"
+                  color="#8D8D8D"
+                  fontSize="1.5em"
+                  fontWeight="bold"
+                  fontSizeMedia="1.2em"
+                  fontSizeMedia950="1.1em"
+                  boxShadow="0 4px 2px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+                  onClick={() => history.push("/web/editarperfil")}
+                >
+                  CANCELAR
+                </Button>
+                <Button
+                  height="50px"
+                  width="40%"
+                  backgroundColor="#434B97"
+                  borderColor="#151B57"
+                  color="white"
+                  fontSize="1.5em"
+                  fontWeight="bold"
+                  fontSizeMedia="1.2em"
+                  fontSizeMedia950="1.1em"
+                  boxShadow="0 4px 2px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+                  onClick={() => history.push("/web/perfil")}
+                >
+                  ALTERAR
+                </Button>
+              </BotoesMesmaLinha>
+            </Caixa>
+          </Conteudo>
+        </div>
+      )}
     </div>
   );
 }
 
 export default AlterarSenha;
-
