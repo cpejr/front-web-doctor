f29c163a088f53ec4d6c228823dbb5f698faa55d
Mateus Pardini <mateuspardini6@gmail.com>
2022-04-29 21:52:28 -0300

    feat: Layout pronto + inicio linkagem back

--- src/pages/PerfilPaciente/PerfilPaciente.js
+++ src/pages/PerfilPaciente/PerfilPaciente.js
@@ -14,34 +14,187 @@ import {
   Anos,
   PerfilEsquerda,
   PerfilDireita,
+  Titulo,
+  Botoes,
+  Botao,
+  Formulario,
+  DadosFormulario,
+  DadosContato,
+  DadosGeo,
+  InfoContato,
+  Receita,
+  BotaoReceita,
+  DadosReceita,
+  TituloFormulario,
+  RespostaPendente,
+  Resposta,
+  TituloReceita,
 } from "./Styles";
 import * as managerService from "../../services/ManagerService/managerService";
-import { LoadingOutlined } from "@ant-design/icons";
+import { LoadingOutlined, StarOutlined, StarFilled } from "@ant-design/icons";
 import { Spin } from "antd";
 import { useHistory } from "react-router-dom";
+import logoGuilherme from "../../assets/logoGuilherme.png";
+import Button from "../../styles/Button";
 
 function PerfilPaciente() {
-  
+  const email = sessionStorage.getItem("@doctorapp-Email");
+  const [usuario, setUsuario] = useState({});
+  const [endereco, setEndereco] = useState({});
+  const [telefone, setTelefone] = useState("");
+  const [dataNascimento, setDataNascimento] = useState("");
+
+  async function pegandoDados() {
+    const resposta = await managerService.GetDadosUsuario(email);
+    setUsuario(resposta.dadosUsuario);
+    setTelefone(resposta.dadosUsuario.telefone);
+    setDataNascimento(resposta.dadosUsuario.data_nascimento);
+    setEndereco(resposta.dadosEndereco);
+  }
+
+  useEffect(() => {
+    pegandoDados();
+  }, []);
+
   return (
     <ContainerPerfil>
       <Perfil>
         <PerfilEsquerda>
           <PerfilSuperior>
-            <FotoPerfil></FotoPerfil>
+            <FotoPerfil>
+              <img src={logoGuilherme} className="foto"></img>
+            </FotoPerfil>
             <Dados>
-              <Nome>teste</Nome>
-              <DataAnos>
-                <Data>teste</Data>
-                <Anos>teste</Anos>
-              </DataAnos>
+              <Nome>{usuario.nome}</Nome>
+              <Data>{telefone}</Data>
             </Dados>
           </PerfilSuperior>
-          <PerfilInferior></PerfilInferior>
+          <PerfilInferior>
+            <Titulo>Endereço:</Titulo>
+            <DadosGeo>{endereco.pais}:</DadosGeo>
+            <DadosGeo>{endereco.estado}:</DadosGeo>
+            <DadosGeo>{endereco.cidade}:</DadosGeo>
+            <DadosGeo>{endereco.cep}:</DadosGeo>
+            <DadosGeo>{endereco.rua}:</DadosGeo>
+            <DadosGeo style={{ color: "green" }}>Apartamento:</DadosGeo>
+          </PerfilInferior>
         </PerfilEsquerda>
-        <PerfilDireita></PerfilDireita>
+        <PerfilDireita>
+          <DadosContato>
+            <Titulo>Contato</Titulo>
+            <InfoContato>Celular:</InfoContato>
+            <InfoContato textDecoration="underline">Email:</InfoContato>
+          </DadosContato>
+          <Botoes>
+            <Botao>
+              <Button
+                backgroundColor="green"
+                color="#0A0E3C"
+                fontWeight="bold"
+                borderColor="#0A0E3C"
+                height="40px"
+                width="100%"
+                fontSize="1.3em"
+              >
+                Iniciar Conversa
+              </Button>
+            </Botao>
+            <Botao>
+              <Button
+                backgroundColor="green"
+                color="#0A0E3C"
+                fontWeight="bold"
+                borderColor="#0A0E3C"
+                height="40px"
+                width="100%"
+                fontSize="1.3em"
+              >
+                Agendamentos
+              </Button>
+            </Botao>
+            <Botao>
+              <Button
+                backgroundColor="green"
+                color="#0A0E3C"
+                fontWeight="bold"
+                borderColor="#0A0E3C"
+                height="40px"
+                width="100%"
+                fontSize="1.3em"
+              >
+                Excluir Usuário
+              </Button>
+            </Botao>
+          </Botoes>
+        </PerfilDireita>
       </Perfil>
-      <Formularios></Formularios>
-      <Receitas></Receitas>
+      <Formularios>
+        <Titulo>FORMULÁRIOS</Titulo>
+        <Formulario>
+          <DadosFormulario>
+            <TituloFormulario
+              textDecoration="underline"
+              color="black"
+              fontSize="1.5em"
+            >
+              Título do Formulário
+            </TituloFormulario>
+            <TituloFormulario color="#434B97" fontSize="1.2em">
+              xx/xx/2022
+            </TituloFormulario>
+            <TituloFormulario color="#434B97" fontSize="1.2em">
+              Tipo:
+            </TituloFormulario>
+            <TituloFormulario color="#434B97" fontSize="1.2em">
+              Urgência <StarOutlined />
+              <StarFilled />
+              <StarFilled />
+            </TituloFormulario>
+          </DadosFormulario>
+          <RespostaPendente>
+            <Resposta>Resposta Pendente</Resposta>
+            <Button
+              backgroundColor="green"
+              color="#0A0E3C"
+              fontWeight="bold"
+              borderColor="#0A0E3C"
+              height="40px"
+              width="100%"
+            >
+              ENVIAR LEMBRETE
+            </Button>
+          </RespostaPendente>
+        </Formulario>
+      </Formularios>
+      <Receitas>
+        <Titulo>RECEITAS</Titulo>
+        <Receita>
+          <DadosReceita>
+            <TituloReceita
+              textDecoration="underline"
+              color="black"
+              fontSize="1.5em"
+            >
+              Título
+            </TituloReceita>
+            <TituloReceita color="#434B97" fontSize="1.2em">
+              xx/xx/2022
+            </TituloReceita>
+          </DadosReceita>
+          <BotaoReceita>
+            <Button
+              backgroundColor="green"
+              color="#0A0E3C"
+              fontWeight="bold"
+              borderColor="#0A0E3C"
+              height="40px"
+              width="100%"
+            >
+              DOWNLOAD
+            </Button>
+          </BotaoReceita>
+        </Receita>
+      </Receitas>
     </ContainerPerfil>
   );
 }
--- src/pages/PerfilPaciente/Styles.js
+++ src/pages/PerfilPaciente/Styles.js
@@ -9,23 +9,193 @@ export const ContainerPerfil = styled.div`
   padding: 2% 5% 2% 5%;
 `;
 
-export const PerfilEsquerda = styled.div``;
-export const PerfilDireita = styled.div``;
+export const PerfilEsquerda = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 50%;
+`;
+export const PerfilDireita = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 50%;
+`;
 export const Perfil = styled.div`
   display: flex;
+  height: 30%;
   flex-direction: row;
   border-style: solid;
-  border-color:#c4c4c4 ;
+  border-color: #c4c4c4;
   border-width: 2px;
-  border-radius:3px;
+  border-radius: 3px;
+  padding: 2%;
+  box-shadow: 3px 3px 5px 0px rgba(0, 0, 0, 0.2);
+  gap: 15%;
+  margin-bottom: 2%;
 `;
-export const Formularios = styled.div``;
-export const Receitas = styled.div``;
-export const PerfilSuperior = styled.div``;
-export const PerfilInferior = styled.div``;
-export const FotoPerfil = styled.div``;
+export const Formularios = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-style: solid;
+  border-color: #c4c4c4;
+  border-width: 2px;
+  border-radius: 3px;
+  height: 30%;
+  margin-bottom: 2%;
+  padding: 2%;
+  box-shadow: 3px 3px 5px 0px rgba(0, 0, 0, 0.2);
+`;
+export const Receitas = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-style: solid;
+  border-color: #c4c4c4;
+  border-width: 2px;
+  border-radius: 3px;
+  padding: 2%;
+  box-shadow: 3px 3px 5px 0px rgba(0, 0, 0, 0.2);
+  height: 30%;
+`;
+export const PerfilSuperior = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 2%;
+  justify-content: space-around;
+  width: 100%;
+`;
+export const PerfilInferior = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2%;
+  justify-content: space-around;
+  width: 100%;
+`;
+export const FotoPerfil = styled.div`
+  .foto {
+    border-radius: 5px;
+    width: 100%;
+    height: 100%;
+  }
+`;
 export const Dados = styled.div``;
-export const Nome = styled.div``;
-export const DataAnos = styled.div``;
-export const Data = styled.div``;
-export const Anos = styled.div``;
+export const Nome = styled.div`
+  width: 100%;
+  font-size: 1.8em;
+  color: #151b57;
+  font-weight: bold;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+export const DataAnos = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  gap: 2%;
+  width: 100%;
+`;
+export const Data = styled.div`
+  font-size: 1.2em;
+  justify-content: flex-start;
+  color: #151b57;
+`;
+export const Anos = styled.div`
+  font-size: 1.2em;
+  color: #151b57;
+`;
+export const Titulo = styled.div`
+  font-size: 1.5em;
+  color: #151b57;
+  font-weight: bold;
+  margin: 0% 0% 2% 0%;
+`;
+export const DadosGeo = styled.div`
+  font-size: 1.2em;
+  color: #151b57;
+  margin-bottom: 2%;
+  text-decoration: ${(props) => props.textDecoration};
+`;
+export const Botoes = styled.div`
+  margin-top: 15%;
+  display: flex;
+  flex-direction: column;
+  padding: 0% 20% 0% 0%;
+  justify-content: start;
+`;
+export const Botao = styled.div`
+  margin-bottom: 5%;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+export const DadosFormulario = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: flex-end;
+`;
+export const DadosContato = styled.div`
+  justify-content: start;
+  padding: 2%;
+`;
+export const InfoContato = styled.div`
+  text-decoration: ${(props) => props.textDecoration};
+  font-size: 1.2em;
+  color: #151b57;
+`;
+export const Formulario = styled.div`
+  height: 140px;
+  width: 100%;
+  border-style: solid;
+  border-color: #151b57;
+  border-width: 2px;
+  border-radius: 3px;
+  background-color: #eaecff;
+`;
+export const Receita = styled.div`
+  height: 140px;
+  width: 100%;
+  border-style: solid;
+  border-color: #151b57;
+  border-width: 2px;
+  border-radius: 3px;
+  background-color: #eaecff;
+`;
+export const DadosReceita = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 1% 10% 0% 10%;
+`;
+export const BotaoReceita = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  padding: 1% 5% 1% 75%;
+`;
+export const TituloFormulario = styled.div`
+  text-decoration: ${(props) => props.textDecoration};
+  color: ${(props) => props.color};
+  font-size: ${(props) => props.fontSize};
+`;
+export const Resposta = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  font-size: 1.5em;
+  width: 100%;
+`;
+export const RespostaPendente = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  padding: 1% 5% 1% 75%;
+`;
+export const TituloReceita = styled.div`
+  text-decoration: ${(props) => props.textDecoration};
+  color: ${(props) => props.color};
+  font-size: ${(props) => props.fontSize};
+`;
