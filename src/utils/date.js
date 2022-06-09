88aa9304163be664bdcf323d6a9c95c125e04041
Maria <mariamsilva15@ufmg.br>
2022-06-08 22:11:20 -0300

    fix: validacao data funcional

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,14 +1,17 @@
 export function brParaPadrao(strDataOriginal){
     const [dia, mes, ano] = strDataOriginal.split("/")
     if (!dia?.length || !mes?.length || !ano?.length) throw new Error()
+    if (mes !== "02"){
+        if ( mes % 2 === 0 && dia > "30" )
+            return "Data Invalida"
+    } else {
+        if ( dia > "28" )
+            return "Data Invalida"
+    }
     const dataFormatada = `${ano.padStart(4,"00")}-${mes.padStart(2,"0")}-${dia.padStart(2,"0")}`
-    // if (new Date(dataFormatada) === "Invalid date") {
-    //     return "Data invalida"
-    // } else {
-    //     return dataFormatada;
-    // }
-    console.log(isNaN(new Date(dataFormatada)))
-    console.log(new Date(dataFormatada))
-
-    //nao pega o invalid date
+    if (isNaN(new Date(dataFormatada)) === false) {
+        return dataFormatada 
+    } else {
+        return "Data Invalida" 
+    }
 }
