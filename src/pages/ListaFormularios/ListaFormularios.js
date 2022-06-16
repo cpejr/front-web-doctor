5b6781f7fa00970a8fa8697f61e32ba473fe301a
Laura <laurapires@cpejr.com.br>
2022-06-16 18:09:46 -0300

    feat: criacao de componentes e estilizacao

--- src/pages/ListaFormularios/ListaFormularios.js
+++ src/pages/ListaFormularios/ListaFormularios.js
@@ -1,9 +1,189 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { Input, Select, Modal } from "antd";
+import {
+  TopoPagina,
+  ContainerListadeUsuarios,
+  Filtros,
+  FiltroDatas,
+  FiltroUsuario,
+  BarraPesquisa,
+  BarraEstetica,
+  ContainerUsuarios,
+  Resposta,
+  RespostaPendente,
+  UrgenciaFormulario,
+  TipoFormulario,
+  TituloFormulario,
+  Formulario,
+  DadosFormulario,
+  BotoesVertical,
+  ContainerFormularioEspecifico,
+} from "./Styles";
+import { Cores } from "../../variaveis";
+import Button from "../../styles/Button";
+import { LoadingOutlined, StarOutlined, StarFilled } from "@ant-design/icons";
+import { Spin } from "antd";
+import * as managerService from "../../services/ManagerService/managerService";
+import ModalAgendamentoEspecifico from "../../components/ModalAgendamentoEspecifico";
+import ModalAdicionarCodigo from "../../components/ModalAdicionarCodigo/ModalAdicionarCodigo";
 
 function ListaFormularios() {
+  const history = useHistory();
+
+  const { Search } = Input;
+  const [usuarios, setUsuarios] = useState([]);
+  const [tipoUsuarioLogado, setTipoUsuarioLogado] = useState([]);
+  const [carregando, setCarregando] = useState(true);
+  const [modalAgendamento, setModalAgendamento] = useState(false);
+  const [emailPaciente, setEmailPaciente] = useState(false);
+  const [modalAdicionarCodigo, setModalAdicionarCodigo] = useState(false);
+  const [email, setEmail] = useState();
+
+  const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+  const emailLogado = sessionStorage.getItem("@doctorapp-Email");
+
+  async function pegandoTipoUsuarioLogado() {
+    const resposta = await managerService.GetDadosUsuario(emailLogado);
+    setTipoUsuarioLogado(resposta.dadosUsuario.tipo);
+  }
+
+  useEffect(() => {
+    pegandoDadosUsuarios();
+  }, [tipoUsuarioLogado]);
+
+  useEffect(() => {
+    pegandoTipoUsuarioLogado();
+  }, []);
+
+  async function pegandoDadosUsuarios() {
+    const resposta = await managerService.GetFormularios();
+    
+      setUsuarios(resposta);
+     
+  }
+
+  async function marcandoAgendamento(emailPaciente) {
+    setEmailPaciente(emailPaciente);
+    setModalAgendamento(true);
+  }
+
+  async function fechandoModal() {
+    setModalAgendamento(false);
+  }
+
+  async function abrindoModalCodigo(email) {
+    setEmail(email);
+    setModalAdicionarCodigo(true);
+  }
+
+  async function fechandoModalCodigo() {
+    setModalAdicionarCodigo(false);
+  }
+
   return (
     <div>
-      <h1>ListaFormularios</h1>
+      <ContainerListadeUsuarios>
+        <TopoPagina>
+          <BarraPesquisa>
+            <Search placeholder="BUSCAR" style={{ width: 400 }} />
+          </BarraPesquisa>
+          <Filtros>
+            <FiltroDatas>
+              <Select
+                defaultValue="Tipos"
+                style={{ color: "green", width: 200 }}
+              ></Select>
+            </FiltroDatas>
+            <FiltroUsuario>
+              <Select
+                defaultValue="Finalidades"
+                style={{ color: "green", width: 200 }}
+              ></Select>
+            </FiltroUsuario>
+            <FiltroDatas>
+              <Select
+                defaultValue="Urgências"
+                style={{ color: "green", width: 200 }}
+              ></Select>
+            </FiltroDatas>
+          </Filtros>
+        </TopoPagina>
+        <BarraEstetica/>
+        <ContainerUsuarios>
+          {usuarios?.map((value) => (
+            <ContainerFormularioEspecifico>
+            <Formulario>
+            <DadosFormulario>
+              <TituloFormulario>{value.titulo}</TituloFormulario>
+              <TipoFormulario>Tipo: {value.tipo}</TipoFormulario>
+              <UrgenciaFormulario>
+                <> Urgência: </>
+                <StarFilled />
+                <StarFilled />
+                <StarOutlined />
+              </UrgenciaFormulario>
+            </DadosFormulario>
+            <RespostaPendente>
+              <Resposta>Resposta Pendente</Resposta>
+            </RespostaPendente>
+              </Formulario>
+            <BotoesVertical>
+            <Button
+                backgroundColor="green"
+                color={Cores.azulEscuro}
+                fontWeight="bold"
+                borderColor={Cores.azulEscuro}
+                height="37px"
+                width="90%"
+              >
+                ENVIAR LEMBRETE
+              </Button>
+              <Button
+                backgroundColor={Cores.azulEscuro}
+                color={Cores.azulEscuro}
+                fontWeight="bold"
+                borderColor={Cores.azulEscuro}
+                height="37px"
+                width="90%"
+              >
+                ENVIAR LEMBRETE
+              </Button>
+              <Button
+                backgroundColor="green"
+                color={Cores.azulEscuro}
+                fontWeight="bold"
+                borderColor={Cores.azulEscuro}
+                height="37px"
+                width="90%"
+              >
+                ENVIAR LEMBRETE
+              </Button>
+              </BotoesVertical>
+              </ContainerFormularioEspecifico>
+          ))}
+        </ContainerUsuarios>
+      </ContainerListadeUsuarios>
+
+      <Modal
+        visible={modalAgendamento}
+        onCancel={fechandoModal}
+        footer={null}
+        width={"70%"}
+        centered={true}
+      >
+        <ModalAgendamentoEspecifico emailUsuario={emailPaciente} />
+      </Modal>
+
+      <Modal
+        visible={modalAdicionarCodigo}
+        onCancel={fechandoModal}
+        footer={null}
+        width={"70%"}
+        centered={true}
+      >
+        <ModalAdicionarCodigo emailUsuario={email} />
+      </Modal>
     </div>
   );
 }
---
+++ src/pages/ListaFormularios/Styles.js
@@ -0,0 +1,325 @@
+import styled from "styled-components";
+import { Cores } from "../../variaveis";
+
+export const ContainerListadeUsuarios = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  padding: 2% 5% 2% 5%;
+`;
+export const TopoPagina = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  @media (max-width: 920px) {
+    flex-direction: column;
+    justify-content: center;
+  }
+`;
+export const BarraPesquisa = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  .ant-input {
+    background-color: green ;
+  }
+  .ant-btn.ant-btn-default.ant-btn-icon-only.ant-input-search-button{
+    background-color: green ;
+  }
+  @media (max-width: 920px) {
+    width: 100%;
+    justify-content: center;
+  }
+`;
+export const Filtros = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 2%;
+  @media (max-width: 920px) {
+    margin-top: 2%;
+    width: 100%;
+    justify-content: center;
+  }
+  @media (max-width: 450px) {
+    margin-top: 2%;
+    flex-direction: column;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+export const FiltroUsuario = styled.div`
+  @media (max-width: 480px) {
+    margin-top: 2%;
+  }
+`;
+export const FiltroDatas = styled.div`
+  @media (max-width: 480px) {
+    margin-top: 2%;
+  }
+`;
+
+export const BarraEstetica = styled.div`
+  margin-top: 2%;
+  margin-bottom: 1%;
+  height: 2px;
+  width: 100%;
+  background-color: ${Cores.azul};
+`;
+export const DadosUsuario = styled.div`
+  color: ${Cores.azul};
+  justify-content: space-around;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const ContainerUsuarios = styled.div`
+  padding: 2% 0% 2% 0%;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+export const Usuario = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 80px;
+  padding: 0% 1% 0% 1%;
+  border-color: ${Cores.preto};
+  border-style: solid;
+  border-radius: 3px;
+  box-shadow: 3px 3px 5px 0px rgba(0, 0, 0, 0.2);
+  margin-bottom: 2%;
+`;
+
+export const Titulo = styled.div`
+  width: 30px;
+  display: flex;
+  justify-content: center;
+  @media (max-width: 880px) {
+    display: none;
+  }
+`;
+export const Imagem = styled.div`
+  justify-content: center;
+  object-fit: fill;
+  width: 10%;
+  width: 40px;
+  display: flex;
+  @media (max-width: 880px) {
+    display: none;
+  }
+`;
+export const Nome = styled.div`
+  width: 18%;
+  justify-content: center;
+
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  :hover {
+    overflow: visible;
+    cursor: pointer;
+  }
+
+  @media (max-width: 880px) {
+    width: 20%;
+  }
+  @media (max-width: 700px) {
+    width: 25%;
+  }
+  @media (max-width: 600px) {
+    width: 33%;
+  }
+  @media (max-width: 500px) {
+    width: 50%;
+  }
+`;
+export const Telefone = styled.div`
+  width: 18%;
+  justify-content: center;
+  display: flex;
+  @media (max-width: 880px) {
+    width: 20%;
+  }
+  @media (max-width: 700px) {
+    display: none;
+  }
+`;
+export const UltimaVisita = styled.div`
+  width: 18%;
+  display: flex;
+  justify-content: center;
+  color: green;
+  @media (max-width: 880px) {
+    width: 20%;
+  }
+  @media (max-width: 700px) {
+    width: 25%;
+  }
+  @media (max-width: 600px) {
+    display: none;
+  }
+`;
+export const BotaoAdicionar = styled.div`
+  width: 18%;
+  display: flex;
+  justify-content: center;
+  color: green;
+  @media (max-width: 880px) {
+    width: 20%;
+  }
+  @media (max-width: 700px) {
+    width: 25%;
+  }
+  @media (max-width: 600px) {
+    width: 33%;
+  }
+  @media (max-width: 500px) {
+    width: 50%;
+  }
+`;
+
+export const CódigoPaciente = styled.div`
+  width: 18%;
+  display: flex;
+  justify-content: center;
+  color: green;
+  @media (max-width: 880px) {
+    width: 20%;
+  }
+  @media (max-width: 700px) {
+    width: 25%;
+  }
+  @media (max-width: 600px) {
+    width: 33%;
+  }
+  @media (max-width: 500px) {
+    display: none;
+  }
+`;
+export const CaixaVazia = styled.div`
+  width: 18%;
+
+  @media (max-width: 880px) {
+    width: 20%;
+  }
+  @media (max-width: 700px) {
+    width: 25%;
+  }
+  @media (max-width: 600px) {
+    width: 33%;
+  }
+  @media (max-width: 500px) {
+    width: 50%;
+  }
+`;
+
+export const DadosFormulario = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: flex-end;
+  margin-bottom: 2%;
+
+  @media (max-width: 560px) {
+    gap: 2%;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+  }
+`;
+export const Formulario = styled.div`
+  width: 83%;
+  min-height: 20vh;
+  display: flex;
+  flex-direction: column;
+  border-style: solid;
+  border-color: ${Cores.azul};
+  border-width: 3px;
+  border-radius: 3px;
+  justify-content: center;
+  padding: 0% 2% 2% 2%;
+  background-color: ${Cores.azulClaro};
+  
+`;
+
+export const BotoesVertical = styled.div`
+  width: 15%;
+  display: flex;
+  min-height: 20vh;
+  justify-content: space-between;
+  flex-direction: column;
+`;
+
+export const ContainerFormularioEspecifico = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  margin: 2% 0% 2% 0%;
+`;
+
+export const TituloFormulario = styled.div`
+  font-size: 1.5em;
+  text-decoration: underline;
+  color: ${Cores.preto};
+
+  @media (max-width: 780px) {
+    font-size: 1.3em;
+  }
+  @media (max-width: 560px) {
+    font-size: 1.1em;
+  }
+`;
+export const DataFormulario = styled.div`
+  font-size: 1.2em;
+  color: ${Cores.lilas[1]};
+
+  @media (max-width: 780px) {
+    display: none;
+  }
+`;
+export const TipoFormulario = styled.div`
+  font-size: 1.2em;
+  color: ${Cores.lilas[1]};
+
+  @media (max-width: 560px) {
+    display: none;
+  }
+`;
+export const UrgenciaFormulario = styled.div`
+  font-size: 1.2em;
+  color: ${Cores.lilas[1]};
+  @media (max-width: 780px) {
+    font-size: 1.3em;
+  }
+`;
+
+export const Resposta = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: end;
+  font-size: 1.3em;
+  width: 100%;
+  @media (max-width: 560px) {
+    align-items: center;
+  }
+`;
+export const RespostaPendente = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  width: 100%;
+`;
