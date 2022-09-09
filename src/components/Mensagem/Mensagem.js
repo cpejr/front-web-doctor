b34274e379981c043f0f9c2bdac3e1a24cd150ab
João Pedro Lima Pirajá <jplp100@hotmail.com>
2022-09-08 22:07:55 -0300

    fix: componente Mensagem.

--- src/components/Mensagem/Mensagem.js
+++ src/components/Mensagem/Mensagem.js
@@ -1,23 +1,18 @@
-import React from "react";
-import dayjs from "dayjs";
-import { MensagemEnviada, DataHoraMensagem } from "./Styles";
-import { format, register } from "timeago.js";
-import pt_BR from "timeago.js/lib/lang/pt_BR";
-
-register("pt_BR", pt_BR);
+import React from 'react';
+import dayjs from 'dayjs';
+import { MensagemEnviada, DataHoraMensagem } from './Styles';
 
 export default function Mensagem({
-	pertenceAoUsuarioAtual,
-	conteudo,
-	data_criacao,
+  pertenceAoUsuarioAtual,
+  conteudo,
+  data_criacao,
 }) {
-	return (
-		<MensagemEnviada pertenceAoUsuarioAtual={pertenceAoUsuarioAtual}>
-			{conteudo}
-			<DataHoraMensagem>
-				{dayjs(data_criacao).format("DD/MM/YYYY HH:mm")}
-			</DataHoraMensagem>
-			{/* <DataHoraMensagem>{format(data_criacao, "pt_BR")}</DataHoraMensagem> */}
-		</MensagemEnviada>
-	);
+  return (
+    <MensagemEnviada pertenceAoUsuarioAtual={pertenceAoUsuarioAtual}>
+      {conteudo}
+      <DataHoraMensagem>
+        {dayjs(data_criacao).format('DD/MM/YYYY HH:mm')}
+      </DataHoraMensagem>
+    </MensagemEnviada>
+  );
 }
