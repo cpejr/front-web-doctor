d99e31208c54f216bfd7dcbd566ff1f1e0b3eaa5
mariana rabelo <marianarabelo@cpejr.com.br>
2022-09-08 23:02:40 -0300

    feat: organizando componentes

--- src/pages/CriacaoReceitas/CriacaoReceitas.js
+++ src/pages/CriacaoReceitas/CriacaoReceitas.js
@@ -2,24 +2,26 @@ import React from "react";
 import Button from "../../styles/Button";
 import Input from "../../styles/Input";
 import Select from "../../styles/Select";
-import { ContainerCriacaoReceitas, CriacaoReceitaNome, CriacaoReceitaCorpo, Titulo, NomeDoPaciente, Descricao, CriacaoReceitaBotoes } from "./Styles";
+import { ContainerCriacaoReceitas, CardCriacaoReceitas, CriacaoReceitaNome, CriacaoReceitaCorpo, Titulo, NomeDoPaciente, Descricao, CriacaoReceitaBotoes } from "./Styles";
 
 function CriacaoReceitas() {
   return (
     <ContainerCriacaoReceitas>
-      <CriacaoReceitaNome>Criação de Receita</CriacaoReceitaNome>
-      <CriacaoReceitaCorpo>
-        <Titulo>Título:</Titulo>
-        <Input></Input>
-        <NomeDoPaciente>Nome do paciente</NomeDoPaciente>
-        <Select></Select>
-        <Descricao>Descrição:</Descricao>
-        <Input></Input>
-      </CriacaoReceitaCorpo>
-      <CriacaoReceitaBotoes>
-        <Button>CANCELAR</Button>
-        <Button>ENVIAR</Button>
-      </CriacaoReceitaBotoes>  
+      <CardCriacaoReceitas>
+        <CriacaoReceitaNome>Criação de Receita</CriacaoReceitaNome>
+        <CriacaoReceitaCorpo>
+          <Titulo>Título:</Titulo>
+          <Input></Input>
+          <NomeDoPaciente>Nome do paciente</NomeDoPaciente>
+          <Select></Select>
+          <Descricao>Descrição:</Descricao>
+          <Input></Input>
+        </CriacaoReceitaCorpo>
+        <CriacaoReceitaBotoes>
+          <Button>CANCELAR</Button>
+          <Button>ENVIAR</Button>
+        </CriacaoReceitaBotoes>  
+      </CardCriacaoReceitas>
     </ContainerCriacaoReceitas>
   );
 }
--- src/pages/CriacaoReceitas/Styles.js
+++ src/pages/CriacaoReceitas/Styles.js
@@ -1,3 +1,50 @@
 import styled from "styled-components";
 import { Cores } from "../../variaveis";
 
+
+
+export const ContainerCriacaoReceitas = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  width: 100%;
+  height: 100%;
+  min-width:200px;
+`;
+
+export const CardCriacaoReceitas = styled.div`
+display: flex;
+flex-direction: column;
+align-items:center;
+border-style:solid ;
+border-width:3px;
+border-color:black;
+padding-left:4%;
+padding-right:4%;
+padding-bottom:1%;
+padding-top:1%;
+`;
+
+export const CriacaoReceitaNome = styled.div`
+
+`;
+
+export const CriacaoReceitaCorpo = styled.div`
+
+`;
+
+export const Titulo = styled.div`
+
+`;
+
+export const NomeDoPaciente = styled.div`
+
+`;
+
+export const Descricao = styled.div`
+
+`;
+
+export const CriacaoReceitaBotoes = styled.div`
+display: flex;
+`;
