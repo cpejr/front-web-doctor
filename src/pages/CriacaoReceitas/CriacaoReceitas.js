d5897b38de00281d3e541c4f0e660eda696e9fc4
mariana rabelo <marianarabelo@cpejr.com.br>
2022-09-10 13:55:10 -0300

    feat: select funcionando

--- src/pages/CriacaoReceitas/CriacaoReceitas.js
+++ src/pages/CriacaoReceitas/CriacaoReceitas.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../../styles/Button";
 import Input from "../../styles/Input";
 import Select from "../../styles/Select";
 import { Cores } from "../../variaveis";
+import { LoadingOutlined } from '@ant-design/icons';
+import { Spin } from "antd";
+import * as managerService from "../../services/ManagerService/managerService";
 import { ContainerCriacaoReceitas, CardCriacaoReceitas, CriacaoReceitaNome, CriacaoReceitaCorpo, Titulo, NomeDoPaciente, Descricao, CriacaoReceitaBotoes } from "./Styles";
 
 function CriacaoReceitas() {
+
+  const [usuarios, setUsuarios] = useState([]);
+  const [carregando, setCarregando] = useState(false);
+  const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
+  async function pegandoPacientes() {
+    setCarregando(true);
+    const resposta = await managerService.GetDadosPessoais();
+    resposta.forEach((usuario) => {
+      if (usuario.tipo === "PACIENTE") {
+        setUsuarios((usuarios) => [...usuarios, usuario]);
+      }
+    });
+  }
+
+  useEffect(() => {
+    pegandoPacientes();
+  }, []);
+  
   return (
     <ContainerCriacaoReceitas>
       <CardCriacaoReceitas>
@@ -20,35 +42,26 @@ function CriacaoReceitas() {
           width="100%"
           marginTop="2%"
           ></Input>
-          <NomeDoPaciente>Nome do paciente</NomeDoPaciente>
+          <NomeDoPaciente>Nome do paciente:</NomeDoPaciente>
           <Select
             backgroundColor={Cores.cinza[7]}
             color={Cores.preto}
             fontSize="1em"
             width="100%"
             marginTop="2%"
+            size="large"
             name="id_usuario"
-            placeholder="Selecione um paciente"
-            onChange={(e) => {
-              validacaoCampos(e);
-            }}
+            placeholder="Nome do usuário"
           >
             <option value="" disabled selected>
-              Paciente
+              Nome do paciente
             </option>
-
             {usuarios.map((usuario) => (
-              <>
-                {carregando ? (
-                  <Spin indicator={antIcon} />
-                ) : (
-                  <option key={usuario.id} value={usuario.id} color="red">
-                    {usuario.nome}
-                  </option>
-                )}
-              </>
+              <option key={usuario.id} value={usuario.id} color="red">
+                {usuario.nome}
+              </option>
             ))}
-          </Select>
+        </Select>
           <Descricao>Descrição:</Descricao>
           <Input
           placeholder="Descrição"
