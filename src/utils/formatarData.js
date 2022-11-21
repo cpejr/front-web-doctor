dfbbb72d4de70b3046cf8c0f55be31bd5012988b
João Pedro Lima Pirajá <jplp100@hotmail.com>
2022-11-20 21:26:16 -0300

    fix: adicionando uma checagem para parâmetro nulo ou undefined na função 'formatarData'.

--- src/utils/formatarData.js
+++ src/utils/formatarData.js
@@ -1,6 +1,7 @@
 import { format, zonedTimeToUtc } from "date-fns-tz";
 
 export default function formatarData({ data, formatacao, fuso }) {
+  if (!!!data) return null; 
   const dataParaFormatar = fuso ? zonedTimeToUtc(data, fuso) : new Date(data);
   return format(dataParaFormatar, formatacao);
 }
