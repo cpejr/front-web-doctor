69ce1754bd4278d3c43b4cc006393b3507109820
Bruno Lages <brunok1lages@gmail.com>
2022-11-27 22:55:59 -0300

    feat: atualização funcional da edição Grupo Amie

--- src/pages/EdicaoGrupoAmieWeb/EdicaoGrupoAmieWeb.js
+++ src/pages/EdicaoGrupoAmieWeb/EdicaoGrupoAmieWeb.js
@@ -1,43 +1,206 @@
 import React, { useEffect, useState } from "react";
-import { LoadingOutlined, UserOutlined } from "@ant-design/icons";
-import { Spin, Menu, Dropdown, Modal } from "antd";
-import { useHistory } from "react-router-dom";
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import { Upload } from "antd";
+
 import {
   ContainerEditarGrupoAmieWeb,
   DadosGrupoAmie,
   Botoes,
-  TheOneAboveAll,
+  TextArea,
   Titulo,
+  TheOneAboveAll,
+  UploadButton,
+  UploadSymbol
+
 } from "./Styles";
-import Input from "../../styles/Input";
 import Button from "../../styles/Button";
-import fotoPerfil from "./../../assets/fotoPerfil.png";
+import { sleep } from "../../utils/sleep";
+
 import { Cores } from "../../variaveis";
 import * as managerService from "../../services/ManagerService/managerService";
-import { brParaPadrao } from "../../utils/date";
+
 import { toast } from "react-toastify";
-import _, { get } from "lodash";
-import { sleep, redirecionamento } from "../../utils/sleep";
-import Select from "../../styles/Select/Select";
-import ModalAlterarFoto from "../../components/ModalAlterarFoto";
-import ModalExcluirFoto from "../../components/ModalExcluirFoto";
+import { CaixaBotaoUpload } from "../../components/ModalAlterarFoto/Styles";
+
 
 function EdicaoGrupoAmieWeb() {
+
+  const [carregando, setCarregando] = useState(false);
+  const [id, setId] = useState();
+  const [imagem_um, setImagem_um] = useState();
+  const [texto, setTexto] = useState('');
+  const [imagem_dois, setImagem_dois] = useState();
+  const [alterou, setAlterou] = useState(false);
+
+
+  async function getAmie() {
+    const res = await managerService.getAmie();
+    console.log(res);
+    setImagem_um(res[0].imagem_um);
+    setTexto(res[0].texto);
+    setImagem_dois(res[0].imagem_dois);
+    setId(res[0].id);
+  }
+
+
+  async function setandoImagens() {
+    const chave1 = imagem_um;
+    const chave2 = imagem_dois;
+    if (chave1 === null || chave1 === "" || chave2 === null || chave2 === "")
+      return;
+    setCarregando(true);
+    const imagem1 = await managerService.GetArquivoPorChave(chave1);
+    const imagem2 = await managerService.GetArquivoPorChave(chave2);
+    setImagem_um(imagem1);
+    setImagem_dois(imagem2);
+    await sleep(1500);
+    setCarregando(false);
+  }
+
+  const getBase64 = (img, callback) => {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+  };
+
+  const uploadButton = (
+    <UploadButton>
+      {carregando ? <LoadingOutlined /> :
+        (
+          <UploadSymbol
+          >
+            <PlusOutlined />
+            <div
+              style={{
+                marginTop: 8,
+              }}
+            >
+              Alterar Imagem
+            </div>
+          </UploadSymbol>
+        )}
+    </UploadButton>
+  );
+
+  async function handleChange1(info) {
+    // Get this url from response in real world.
+    setCarregando(true);
+    getBase64(info.file.originFileObj, (url) => {
+      setCarregando(false);
+      setImagem_um(url);
+      setAlterou(true);
+    });
+  }
+
+  async function handleChange2(info) {
+    // Get this url from response in real world.
+    setCarregando(true);
+    getBase64(info.file.originFileObj, (url) => {
+      setCarregando(false);
+      setImagem_dois(url);
+      setAlterou(true);
+    });
+  }
+
+  async function updateAmie() {
+    if (alterou) {
+      await managerService.UpdateAmie(id, imagem_um, texto, imagem_dois);
+    } else {
+      toast.error('Selecione uma foto para editar!');
+    }
+  }
+
+  const beforeUpload = (file) => {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+
+    if (!isJpgOrPng) {
+      toast.error('You can only upload JPG/PNG file!');
+      setCarregando(true);
+    }
+
+    const isLt2M = file.size / 1024 / 1024 < 2;
+
+    if (!isLt2M) {
+      toast.error('Image must smaller than 2MB!');
+      setCarregando(true);
+    }
+
+    return isJpgOrPng && isLt2M;
+  };
+
+  useEffect(() => {
+    getAmie();
+  }, []);
+
+  useEffect(() => {
+    console.log(texto)
+  }, [texto]);
+
+  useEffect(() => {
+    setandoImagens()
+  }, [id]);
 
   return (
     <TheOneAboveAll>
       <ContainerEditarGrupoAmieWeb>
-        <Titulo
-        
-        >
+        <Titulo>
           Página Grupo AMIE
         </Titulo>
 
         <DadosGrupoAmie>
-          <Titulo>
-            ALODFUS
-          </Titulo>
-          <Input />
+          <Upload
+            name='imagem1'
+            listType='picture'
+            showUploadList={false}
+            beforeUpload={beforeUpload}
+            onChange={handleChange1}
+          >
+            {!carregando ? (
+              <img
+                src={imagem_um}
+                alt='imagem1'
+                style={{
+                  maxHeight: '300px',
+                  width: '100%',
+                  height: '100%',
+                }}
+              />
+            ) : ('')}
+            {uploadButton}
+          </Upload>
+          <TextArea
+            value={texto}
+            onChange={(event) => {
+              setTexto(event.target.value)
+              if (!alterou) {
+                setAlterou(true)
+              }
+            }
+            }
+          ></TextArea>
+          <CaixaBotaoUpload>
+            <Upload
+              name='avatar'
+              listType='picture'
+              showUploadList={false}
+              beforeUpload={beforeUpload}
+              onChange={handleChange2}
+            >
+              {!carregando ? (
+                <img
+                  src={imagem_dois}
+                  alt='imagem2'
+                  style={{
+                    maxHeight: '300px',
+                    width: '100%',
+                    height: '100%',
+                  }}
+                />
+              ) : ('')
+              }
+              {uploadButton}
+            </Upload>
+          </CaixaBotaoUpload>
         </DadosGrupoAmie>
 
         <Botoes>
@@ -47,7 +210,8 @@ function EdicaoGrupoAmieWeb() {
             backgroundColor={Cores.cinza[2]}
             color={Cores.branco}
             fontSize='24px'
-            >
+            onClick={() => (updateAmie())}
+          >
             Salvar Alterações
           </Button>
           <Button
--- src/pages/EdicaoGrupoAmieWeb/Styles.js
+++ src/pages/EdicaoGrupoAmieWeb/Styles.js
@@ -2,10 +2,8 @@ import styled from "styled-components";
 import { Cores, Fontes } from "../../variaveis";
 
 export const ContainerEditarGrupoAmieWeb = styled.div`
-  font-family: Barlow;
-  font-weight: 500;
-  height:80%;
   width: 80%;
+  padding: 30px;
   border-color: ${Cores.preto};
   border-radius: 3px;
   border-style: solid;
@@ -14,16 +12,37 @@ export const ContainerEditarGrupoAmieWeb = styled.div`
   justify-content: space-around;
   align-items: center;
   flex-direction:column;
-`
+  row-gap: 40px;
+  @media (max-width: 600px) {
+    width: 100%;
+  }
+
+  `
 
 export const DadosGrupoAmie = styled.div`
+  padding: 30px;
   display: flex;
+  height: 60%;
+  width: 40%;
   align-items: center;
   justify-content: center;
   flex-direction:column;
-  padding: 20px;
+  row-gap: 30px;
   box-shadow: 1px 2px 4px 2px rgba(0, 0, 0, 0.2);
+  @media (max-width: 1200px) {
+    width: 60%;
+  }
+  @media (max-width: 800px) {
+    width: 80%;
+  }
+  @media (max-width: 600px) {
+    width: 100%;
+  }
+  `
+
+export const CaixaBotaoUpload = styled.div`
 `
+
 
 export const Botoes = styled.div`
   display: flex;
@@ -32,14 +51,46 @@ export const Botoes = styled.div`
   flex-direction:column;
 `
 
+export const TextArea = styled.textarea`
+  font-size: 16px;
+  width: 80%;
+  max-height: 500px;
+  overflow:hidden;
+  @media (max-width: 600px) {
+    width: 100%;
+  }
+`
 
 export const Titulo = styled.div`
   font-size: 40px;
+  color: black;
+  font-weight: 500;
+  text-align:center;
+  `
+
+export const UploadButton = styled.div`
+  display: flex;
+  align-items:center;
+  justify-content: center;
 `
+
+export const UploadSymbol = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  justify-content: center;
+  outline: 1px black solid;
+  padding: 5px;
+`
+
 export const TheOneAboveAll = styled.div`
-  width: 100vw;
-  height: 100vh;
+font-family: Barlow;
+  width: 100%;
+  padding: 50px;
   display: flex;
   align-items: center;
   justify-content: center;
+  @media (max-width: 400px) {
+    padding: 20px;
+  }
 `
