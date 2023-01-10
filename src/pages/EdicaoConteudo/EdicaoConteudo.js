5ce28935996b13e063f4f7a619834dfdece93ddb
Amanda Fernandes <amandaalves@cpejr.com.br>
2023-01-10 13:10:21 -0300

    feat: Estilização da página

--- src/pages/EdicaoConteudo/EdicaoConteudo.js
+++ src/pages/EdicaoConteudo/EdicaoConteudo.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Container } from "./Styles";
+import { BotaoGenerico } from "./Styles";
+import { TituloPaginaEdicao } from "./Styles";
 
 function EdicaoConteudo() {
   return (
     <Container>
-      <div>
-        <p>Selecione a página do aplicativo que deseja editar</p>
-        <button>Home</button>
-        <button>Sobre mim</button>
-        <button>Comentários</button>
-        <button>Grupo AIME</button>
-        <button>Indicações e Sugestões</button>
-      </div>
+        <TituloPaginaEdicao>Selecione a página do aplicativo que deseja editar</TituloPaginaEdicao>
+        <BotaoGenerico>Home</BotaoGenerico>
+        <BotaoGenerico>Sobre mim</BotaoGenerico>
+        <BotaoGenerico>Comentários</BotaoGenerico>
+        <BotaoGenerico>Grupo AIME</BotaoGenerico>
+        <BotaoGenerico>Indicações e Sugestões</BotaoGenerico>
     </Container>
   );
 }
--- src/pages/EdicaoConteudo/Styles.js
+++ src/pages/EdicaoConteudo/Styles.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import { Cores, Fontes } from "../../variaveis";
+
 export const Container = styled.div`
-    margin: 30%;
     border: solid 1px black;
     box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.25);
-    width: 35%;
+    width: 652px;
     height: 600px;
     display: flex;
     flex-direction: column;
@@ -12,5 +12,39 @@ export const Container = styled.div`
     justify-content: center;
     margin-top: 119px;
     font-weight: 400;
+    @media (max-width: 780px) {
+    width: 85%;
+  }
+`
+export const TituloPaginaEdicao = styled.p`
+    padding-left: 20%;
+    padding-right: 20%;
+    font-size: 30px;
+    padding-bottom: 18px;
+    text-align: center;
+    font-family: 'Roboto Condensed', sans-serif;
+    @media (max-width: 780px) {
+    font-size: 20px;
+  }
+  @media (max-width: 360px) {
+    font-size: 15px;
+  }
+`
+export const BotaoGenerico = styled.button`
+    background-color: #CFD3F8;
+    font-size: 20px;
+    height: 61px;
+    width: 76%;
+    margin-bottom: 31px;
+    border-radius: 6px;
+    border: solid 3px #BBC0F4;
+    font-family: 'Roboto Condensed', sans-serif;
+    @media (max-width: 780px) {
+    font-size: 15px;
+  }
+  @media (max-width: 360px) {
+    font-size: 12px;
+    height: 40px;
+  }
 `
 
