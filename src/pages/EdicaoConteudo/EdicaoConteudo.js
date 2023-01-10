c71d0b2d757329fe04ea80e472d24d354d4684e6
Amanda Fernandes <amandaalves@cpejr.com.br>
2023-01-10 11:27:31 -0300

    feat: Criação da Página

--- src/pages/EdicaoConteudo/EdicaoConteudo.js
+++ src/pages/EdicaoConteudo/EdicaoConteudo.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Container } from "./Styles";
 
 function EdicaoConteudo() {
   return (
-    <div>
-      <h1>EdicaoConteudo</h1>
-    </div>
+    <Container>
+      <div>
+        <p>Selecione a página do aplicativo que deseja editar</p>
+        <button>Home</button>
+        <button>Sobre mim</button>
+        <button>Comentários</button>
+        <button>Grupo AIME</button>
+        <button>Indicações e Sugestões</button>
+      </div>
+    </Container>
   );
 }
 
---
+++ src/pages/EdicaoConteudo/Styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { Cores, Fontes } from "../../variaveis";
+export const Container = styled.div`
+    margin: 30%;
+    border: solid 1px black;
+    box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.25);
+    width: 35%;
+    height: 600px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 119px;
+    font-weight: 400;
+`
+
