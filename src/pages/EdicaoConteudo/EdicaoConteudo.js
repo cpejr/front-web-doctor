254e6d152bf5c6ca17e9ad413ce5f0cb083d1442
Amanda Fernandes <amandaalves@cpejr.com.br>
2023-01-11 22:03:00 -0300

    feat: Responsividade e linkagem da Página de Edição de Conteúdo

--- src/pages/EdicaoConteudo/EdicaoConteudo.js
+++ src/pages/EdicaoConteudo/EdicaoConteudo.js
@@ -1,18 +1,50 @@
 import React from "react";
 import { Container } from "./Styles";
 import { BotaoGenerico } from "./Styles";
+import { BotaoSemLinkagem } from "./Styles";
 import { TituloPaginaEdicao } from "./Styles";
+import { Corpo } from "./Styles";
+import { useHistory } from 'react-router-dom';
 
 function EdicaoConteudo() {
+  const history = useHistory();
   return (
-    <Container>
-        <TituloPaginaEdicao>Selecione a página do aplicativo que deseja editar</TituloPaginaEdicao>
-        <BotaoGenerico>Home</BotaoGenerico>
-        <BotaoGenerico>Sobre mim</BotaoGenerico>
-        <BotaoGenerico>Comentários</BotaoGenerico>
-        <BotaoGenerico>Grupo AIME</BotaoGenerico>
-        <BotaoGenerico>Indicações e Sugestões</BotaoGenerico>
-    </Container>
+    <Corpo>
+      <Container>
+          <TituloPaginaEdicao>Selecione a página do aplicativo que deseja editar</TituloPaginaEdicao>
+
+          <BotaoGenerico
+          onClick={() => {
+            history.push('/web/Home');
+          }}
+          >Home</BotaoGenerico>
+
+          <BotaoSemLinkagem
+          /*</Container>onClick={() => {
+            history.push('/web/Home');
+          }}*/
+          >Sobre mim</BotaoSemLinkagem>
+
+          <BotaoSemLinkagem
+          /*onClick={() => {
+            history.push('/web/Home');
+          }}*/
+          >Comentários</BotaoSemLinkagem>
+
+          <BotaoSemLinkagem
+          /*onClick={() => {
+            history.push('/web/Home');
+          }}*/
+          >Grupo AIME</BotaoSemLinkagem>
+
+          <BotaoSemLinkagem
+          /*onClick={() => {
+            history.push('/web/Home');
+          }}*/
+          >Indicações e Sugestões</BotaoSemLinkagem>
+
+      </Container>
+    </Corpo>
   );
 }
 
--- src/pages/EdicaoConteudo/Styles.js
+++ src/pages/EdicaoConteudo/Styles.js
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 import { Cores, Fontes } from "../../variaveis";
 
+export const Corpo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+`
 export const Container = styled.div`
     border: solid 1px black;
     box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.25);
@@ -14,20 +21,28 @@ export const Container = styled.div`
     font-weight: 400;
     @media (max-width: 780px) {
     width: 85%;
+    @media (max-width: 360px) {
+    height: 450px;
+  }
   }
 `
 export const TituloPaginaEdicao = styled.p`
     padding-left: 20%;
     padding-right: 20%;
-    font-size: 30px;
+    padding-top: 18px;
     padding-bottom: 18px;
+    font-size: 30px;
     text-align: center;
     font-family: 'Roboto Condensed', sans-serif;
+
     @media (max-width: 780px) {
     font-size: 20px;
+    padding-left: 10%;
+    padding-right: 10%;
   }
   @media (max-width: 360px) {
     font-size: 15px;
+    padding-top: 0px;
   }
 `
 export const BotaoGenerico = styled.button`
@@ -39,12 +54,33 @@ export const BotaoGenerico = styled.button`
     border-radius: 6px;
     border: solid 3px #BBC0F4;
     font-family: 'Roboto Condensed', sans-serif;
+
     @media (max-width: 780px) {
     font-size: 15px;
   }
   @media (max-width: 360px) {
     font-size: 12px;
     height: 40px;
+    margin-bottom: 20px;
+  }
+`
+export const BotaoSemLinkagem = styled.button`
+    background-color: green;
+    font-size: 20px;
+    height: 61px;
+    width: 76%;
+    margin-bottom: 31px;
+    border-radius: 6px;
+    border: solid 3px #BBC0F4;
+    font-family: 'Roboto Condensed', sans-serif;
+
+    @media (max-width: 780px) {
+    font-size: 15px;
+  }
+  @media (max-width: 360px) {
+    font-size: 12px;
+    height: 40px;
+    margin-bottom: 20px;
   }
 `
 
