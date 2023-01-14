ae51e1593dcfb75c29341c5f60bba2d993f135a3
Amanda Fernandes <amandaalves@cpejr.com.br>
2023-01-14 19:06:50 -0300

    feat: Correções página de edição de conteúdo

--- src/pages/EdicaoConteudo/Styles.js
+++ src/pages/EdicaoConteudo/Styles.js
@@ -17,14 +17,20 @@ export const Container = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    margin-top: 119px;
+    margin-top: 7%;
+    margin-bottom: 7%;
     font-weight: 400;
+
+    @media (max-width: 900px) {
+    margin-top: 10%;
+    margin-bottom: 10%;
+   }
     @media (max-width: 780px) {
     width: 85%;
-    @media (max-width: 360px) {
+    }
+   @media (max-width: 360px) {
     height: 450px;
-  }
-  }
+   }
 `
 export const TituloPaginaEdicao = styled.p`
     padding-left: 20%;
@@ -34,53 +40,63 @@ export const TituloPaginaEdicao = styled.p`
     font-size: 30px;
     text-align: center;
     font-family: 'Roboto Condensed', sans-serif;
-
+    
+    @media (max-width: 479px) {
+      font-size: 27px;
+  }
     @media (max-width: 780px) {
-    font-size: 20px;
     padding-left: 10%;
     padding-right: 10%;
-  }
-  @media (max-width: 360px) {
-    font-size: 15px;
-    padding-top: 0px;
-  }
+    }
+    @media (max-width: 431px) {
+      font-size: 25px;
+    }
+    @media (max-width: 400px) {
+      font-size: 22px;
+    }
+    @media (max-width: 360px) {
+      font-size: 18px;
+      padding-top: 0px;
+    }
 `
 export const BotaoGenerico = styled.button`
     background-color: #CFD3F8;
     font-size: 20px;
-    height: 61px;
+    height: 10%;
     width: 76%;
-    margin-bottom: 31px;
+    margin-bottom: 4%;
     border-radius: 6px;
     border: solid 3px #BBC0F4;
     font-family: 'Roboto Condensed', sans-serif;
 
-    @media (max-width: 780px) {
-    font-size: 15px;
+  @media (max-width: 600px) {
+    margin-bottom: 5%;
+  }
+  @media (max-width: 425px) {
+    font-size: 18px;
   }
   @media (max-width: 360px) {
-    font-size: 12px;
-    height: 40px;
-    margin-bottom: 20px;
+    font-size: 14px;
   }
 `
 export const BotaoSemLinkagem = styled.button`
     background-color: green;
     font-size: 20px;
-    height: 61px;
+    height: 10%;
     width: 76%;
-    margin-bottom: 31px;
+    margin-bottom: 4%;
     border-radius: 6px;
     border: solid 3px #BBC0F4;
     font-family: 'Roboto Condensed', sans-serif;
-
-    @media (max-width: 780px) {
-    font-size: 15px;
+    
+    @media (max-width: 600px) {
+    margin-bottom: 5%;
   }
-  @media (max-width: 360px) {
-    font-size: 12px;
-    height: 40px;
-    margin-bottom: 20px;
+    @media (max-width: 425px) {
+    font-size: 18px;
+  }
+    @media (max-width: 360px) {
+    font-size: 14px;
   }
 `
 
